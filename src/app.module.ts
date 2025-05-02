3071c5083c684056885a834b09d56af28e02dcd1
axilfum7 <muflixarahmatullayeva@gmail.com>
2025-05-02 15:52:19 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { MulterModule } from './multer/multer.module';
+import { RegionModule } from './region/region.module';
+import { CategoryModule } from './category/category.module';
+import { ColorModule } from './color/color.module';
+import { ProductModule } from './product/product.module';
+import { LikeModule } from './like/like.module';
+import { OrderModule } from './order/order.module';
+import { CommentModule } from './comment/comment.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, MulterModule, RegionModule,  CategoryModule, ColorModule, ProductModule, LikeModule, OrderModule, CommentModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
